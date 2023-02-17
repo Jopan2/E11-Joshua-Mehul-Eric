63eba188b832a25db50a76abd80396815ff2bf9a
mehnai <71114717+mehnai@users.noreply.github.com>
2023-02-17 15:08:20 -0800

    file

---
+++ testingdata.py
@@ -0,0 +1,14 @@
+import time 
+import random
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--runtime', type= float, required= False)
+args = parser.parse_args()
+a = time.time() + args.runtime
+while time.time()<a:
+    itime = int(time.time())
+    value = random.random()
+    print(itime, value)
+    time.sleep(1)
+
